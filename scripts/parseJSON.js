1e58427b7830f9467ec285d7284c3518faee6ec2
Gwedoh <anthony.d.morano@gmail.com>
2016-12-14 10:43:20 -0600

    AJAX load with function

--- scripts/parseJSON.js
+++ scripts/parseJSON.js
@@ -1,32 +1,37 @@
-var request;
+var mybutton = document.getElementById('loadButton');
+
+mybutton.onclick = function ()
+{
+    var request;
 
 //Check for compatibility
-if (window.XMLHttpRequest)
-{
-    request = new XMLHttpRequest()
-}
-else
-{
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-}
+    if (window.XMLHttpRequest)
+    {
+        request = new XMLHttpRequest()
+    }
+    else
+    {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
 
 //Ajax Request
-request.open('GET', 'data.json');
-request.onreadystatechange = function ()
-{
-    //Check Status of request
-    if ((request.status === 200) && (request.readyState === 4))
+    request.open('GET', 'data.json');
+    request.onreadystatechange = function ()
     {
-        var items = JSON.parse(request.responseText);
-        var output = '<ul>';
-        for (var key in items)
+        //Check Status of request
+        if ((request.status === 200) && (request.readyState === 4))
         {
-            output += '<li>' + items[key].name + '</li>';
+            var items = JSON.parse(request.responseText);
+            var output = '<ul>';
+            for (var key in items)
+            {
+                output += '<li>' + items[key].name + '</li>';
+            }
+            output += '</ul>';
+
+            document.getElementById('update').innerHTML = output;
         }
-        output += '</ul>';
+    };
+    request.send();
 
-        document.getElementById('update').innerHTML = output;
-    }
 };
-request.send();
-
